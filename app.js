098c968200e28719a05ed7016968366bcaa76d28
Stuart Abel <stu822@gmail.com>
2023-07-06 14:48:22 -0500

    add quiz functionality

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ const reviewCard = document.querySelector(".review");
 const startingCard = document.querySelector(".starting-card");
 const reviewEnglish = document.querySelector("#review-english");
 const reviewtranslated = document.querySelector("#review-translated");
-let reviewCardIdx = 2;
+const quizCard = document.querySelector(".quiz");
+const quizForm = document.querySelector("#quiz-form");
+const quizWord = document.querySelector("#quiz-word");
+const selections = document.querySelector("#selections");
 const reviewPrevBtn = document.querySelector("#review-prev");
 const reviewNextBtn = document.querySelector("#review-next");
 const reviewDeckBtn = document.querySelector("#review-deck");
+const startGamebtn = document.querySelector("#start");
+const nextBtn = document.querySelector("#next");
+const finishGameBtn = document.querySelector("#finish");
+const submitBtn = document.querySelector("#quiz-form button[type=submit]");
+const correctAnswer = document.querySelector("#correct-answer");
+const finalScore = document.querySelector("#final-score");
+let reviewCardIdx = 2;
+let cardIdx = [];
+let currentCard;
+let usedChoiceIdx;
+let score;
 deckForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const englishInput = e.target[0];
@@ -38,6 +52,7 @@ finishDeckBtn.addEventListener("click", function () {
 addDeckBtn.addEventListener("click", () => {
   addDeckCard.classList.add("active");
   startingCard.classList.remove("active");
+  quizCard.classList.remove("active");
 });
 
 reviewDeckBtn.addEventListener("click", displayReview);
@@ -55,11 +70,104 @@ reviewNextBtn.addEventListener("click", function () {
     reviewtranslated.textContent = `${deck[reviewCardIdx].translated}`;
   }
 });
+startGamebtn.addEventListener("click", function () {
+  addDeckCard.classList.remove("active");
+  reviewCard.classList.remove("active");
+  quizCard.classList.add("active");
+  cardIdx = [];
+  score = 0;
+  const oldSelections = document.querySelectorAll("#selections div");
+  for (let i = 0; i < oldSelections.length; i++) {
+    oldSelections[i].remove();
+  }
+  createNewCard();
+});
 
+quizForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const selectedChoice = document.querySelector(
+    "input[name=multiple-choice]:checked"
+  );
+  if (!selectedChoice) {
+    console.log("No choice Selected");
+  } else {
+    if (selectedChoice.value === currentCard.translated) {
+      correctAnswer.textContent = "Correct!";
+      score++;
+    } else {
+      console.log("Boo!");
+      correctAnswer.textContent = `Correct Answer: ${currentCard.translated}`;
+    }
+  }
+  submitBtn.disabled = true;
+  if (cardIdx.length === deck.length) {
+    nextBtn.disabled = true;
+    finishGameBtn.disabled = false;
+  } else {
+    nextBtn.disabled = false;
+  }
+});
+
+nextBtn.addEventListener("click", function () {
+  submitBtn.disabled = false;
+  const oldSelections = document.querySelectorAll("#selections div");
+  for (let i = 0; i < oldSelections.length; i++) {
+    oldSelections[i].remove();
+  }
+  correctAnswer.textContent = "";
+  createNewCard();
+  nextBtn.disabled = true;
+});
+
+finishGameBtn.addEventListener("click", function () {
+  finalScore.textContent = `You got ${score} out of ${
+    deck.length
+  }, or ${Math.round((score / deck.length) * 100)}% correct`;
+});
+// Functions
 function displayReview() {
   addDeckCard.classList.remove("active");
+  quizCard.classList.remove("active");
   reviewCard.classList.add("active");
   reviewCardIdx = 0;
   reviewEnglish.textContent = `${deck[0].english}`;
   reviewtranslated.textContent = `${deck[0].translated}`;
+}
+
+function createSections() {
+  usedChoiceIdx = [];
+  for (let i = 0; i < deck.length; i++) {
+    const radioGroup = document.createElement("div");
+    const answer = deck[randomIdx(deck, usedChoiceIdx)].translated;
+    const choice = document.createElement("input");
+    const choiceLabel = document.createElement("label");
+
+    choiceLabel.setAttribute("for", `choice--${i}`);
+    choiceLabel.textContent = `${answer}`;
+
+    choice.setAttribute("type", "radio");
+    choice.setAttribute("value", answer);
+    choice.setAttribute("name", "multiple-choice");
+    choice.setAttribute("id", `choice--${i}`);
+
+    selections.appendChild(radioGroup);
+    radioGroup.appendChild(choice);
+    radioGroup.appendChild(choiceLabel);
+  }
+}
+
+function randomIdx(arr, usedIdx) {
+  let num = Math.floor(Math.random() * arr.length);
+  while (usedIdx.some((idx) => idx === num)) {
+    num = Math.floor(Math.random() * arr.length);
+  }
+  usedIdx.push(num);
+  return num;
+}
+
+function createNewCard() {
+  currentCard = deck[randomIdx(deck, cardIdx)];
+  quizWord.textContent = currentCard.english;
+
+  createSections();
 }
